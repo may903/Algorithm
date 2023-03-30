3887617c185154bfc2eff6560a3bc57d251da5b3
may903 <128146285+may903@users.noreply.github.com>
2023-03-31 00:13:32 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if (n % Math.sqrt(n) == 0){
+            return 1;
+        }else {
+        
+        return 2;}
+    }
+}
